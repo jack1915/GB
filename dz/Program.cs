ab45b15be6dbba5d2c1d6d5307c4c4b89bba4fd4
jack1915 <109028048+jack1915@users.noreply.github.com>
2022-09-14 09:45:02 +0500

    create read.my1

--- dz/Program.cs
+++ dz/Program.cs
@@ -1,27 +1,29 @@
 ﻿// Create read.my
-string[] array1 = new string[5] ;
+string[] array1 = new string[5];
 string[] array2 = new string[array1.Length];
 Console.WriteLine("Введите массив из 5 строк :");
 
-      for (int i = 0;i < array1.Length;i++)
-      {
-        Console.Write("строка[{0}] = ", i);
-        array1[i] = Console.ReadLine();
-      }
-     void PrintMass(string[] array1){
-        for (int i = 0;i < array1.Length;i++){
-           Console.Write($"{array1[i]}" + "   "); 
-        }
-     }
+for (int i = 0; i < array1.Length; i++)
+{
+    Console.Write("строка[{0}] = ", i);
+    array1[i] = Console.ReadLine();
+}
+void PrintMass(string[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        Console.Write($"{array1[i]}" + " ");
+    }
+}
 void Second(string[] array1, string[] array2)
 {
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
-    if(array1[i].Length <= 3)
+        if (array1[i].Length <= 3)
         {
-        array2[count] = array1[i];
-        count++;
+            array2[count] = array1[i];
+            count++;
         }
     }
 }
